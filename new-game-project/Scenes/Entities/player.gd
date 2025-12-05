eea36d2294327c78787c2e8dfe16c6337cb77b0f
Sleety <sleetywashere@gmail.com>
2025-12-05 14:54:44 +0300

    vampire transformation

--- new-game-project/Scenes/Entities/player.gd
+++ new-game-project/Scenes/Entities/player.gd
@@ -1,13 +1,18 @@
 extends CharacterBody2D
 
 @export var Base_Speed = 300.0
+@export var flySpeed : float = 400.0
 var Speed = Base_Speed
+
 @export var Health : int
+@export var isVampire : bool = false
+
 
 func _physics_process(_delta: float) -> void:
 	var direction := Input.get_vector("go_left", "go_right", "go_up", "go_down")
 	if direction:
 		velocity = direction * Speed
+		transform()
 	else:
 		velocity = Vector2.ZERO
 
@@ -15,6 +20,23 @@ func _physics_process(_delta: float) -> void:
 		getHurt(25);
 	
 	move_and_slide()
+
+func transform():
+	if Input.is_action_just_pressed("ui_accept"):
+		if isVampire:
+			Speed = flySpeed
+			$Bat.visible = true
+			$vampire.visible = false
+			isVampire = false
+		else:
+			isVampire = true
+			Speed = Base_Speed
+			$Bat.visible = false
+			$vampire.visible = true
+
+func bite():
+	#if is in area and prey health is less than a amount be able to bite 
+	pass
 
 func getHurt(dmg:int):
 	Health -= dmg
