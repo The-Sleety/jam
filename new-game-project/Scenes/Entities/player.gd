74193355d6aaeff188288f32f3bbd27f381936c1
Egemen12344556f <112466102+Egemen12344556f@users.noreply.github.com>
2025-12-05 14:58:55 +0300

    Game Over Screen
    Added a game over screen with an health bar and respawning

--- new-game-project/Scenes/Entities/player.gd
+++ new-game-project/Scenes/Entities/player.gd
@@ -3,10 +3,10 @@ extends CharacterBody2D
 @export var Base_Speed = 300.0
 var Speed = Base_Speed
 @export var Health : int
-
+var IsDead : bool
 func _physics_process(_delta: float) -> void:
 	var direction := Input.get_vector("go_left", "go_right", "go_up", "go_down")
-	if direction:
+	if direction and !IsDead:
 		velocity = direction * Speed
 	else:
 		velocity = Vector2.ZERO
@@ -19,5 +19,10 @@ func _physics_process(_delta: float) -> void:
 func getHurt(dmg:int):
 	Health -= dmg
 	if Health <= 0:
-		get_tree().reload_current_scene()
-		#ölüm ekran ekle
+		gameOver()
+
+func gameOver():
+	IsDead = true
+	$CanvasLayer/Label2.visible = true
+	$CanvasLayer/Button.visible = true
+	
