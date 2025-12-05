25eecf716af81cadb8e0da411d0dda56cbd61fb5
Egemen12344556f <112466102+Egemen12344556f@users.noreply.github.com>
2025-12-05 19:36:39 +0300

    Egemen Fixes
    neegaaa

--- new-game-project/Scenes/Entities/player.gd
+++ new-game-project/Scenes/Entities/player.gd
@@ -5,48 +5,57 @@ extends CharacterBody2D
 var Speed = Base_Speed
 var IsDead : bool = false
 @export var Health : int
-@export var isVampire : bool = false
-@export var canHit : bool = true
+@export var isVampire : bool = true
 @export var damage : float = 10
-var canBeBitten = true
+var canBite = true
+@export var knocback_strength: int = 5
+
+
+
 func _physics_process(_delta: float) -> void:
 	var direction := Input.get_vector("go_left", "go_right", "go_up", "go_down")
 	if direction:
 		velocity = direction * Speed
-		transform()
 	else:
 		velocity = Vector2.ZERO
 		if isVampire:
-			pass
+			$vampire.play("idle")
 		else:
 			$Bat.play("idle")
 	move_and_slide()
 	
 	if Input.is_action_just_pressed("hit"):
 		bite()
+	
+	if Input.is_action_just_pressed("transform"):
+		transform()
+	print(velocity)
 
 func transform():
-	if Input.is_action_just_pressed("transform"):
 		if isVampire:
+			$VampireCollision.disabled = false
+			$BatCollision.disabled = true
 			Speed = flySpeed
 			$Bat.visible = false
 			$vampire.visible = true
 			isVampire = false
 		else:
+			$BatCollision.disabled = false
+			$VampireCollision.disabled = true
 			isVampire = true
 			Speed = Base_Speed
 			$Bat.visible = true
 			$vampire.visible = false
 
 func bite():
-	if canBeBitten:
+	if canBite:
 		if $HitArea.get_overlapping_bodies().size() > 0:
 			for body in $HitArea.get_overlapping_bodies():
 				if body.is_in_group("Enemy"):
-					if canHit:
-						body.getHurt()
-						canHit = false
+						body.getHurt(randf_range(40,60))
+						print(body.Health)
 						$HitCooldown.start()
+						canBite = false
 	else:
 		return
 
@@ -58,3 +67,6 @@ func getHurt(dmg:int):
 func die():
 	$CanvasLayer/DeathScreen.show()
 	get_tree().paused = true
+
+func _on_hit_cooldown_timeout() -> void:
+	canBite = true
--- new-game-project/Scripts/Entities/villager.gd
+++ new-game-project/Scripts/Entities/villager.gd
@@ -3,31 +3,43 @@ extends CharacterBody2D
 
 var Speed = randf_range(20, 50)
 var canHit = true
-var PlayerBody = null
-var damage = randi_range(10,20)
 var cooldown = 2.4
-var maxHealth = 1
+var maxHealth = randf_range(80,120)
 var Health = maxHealth
 var canMove = true
+
 func _physics_process(_delta: float) -> void:
 	if canMove:
-		position += (get_tree().get_first_node_in_group("Player").global_position - position) / Speed
-	move_and_slide()
-
+		#position += (get_tree().get_first_node_in_group("Player").global_position - position) / Speed
+		pass
+		
 	if $HitArea.get_overlapping_bodies().size() > 0:
 		for body in $HitArea.get_overlapping_bodies():
 			if body.is_in_group("Player"):
 				if canHit:
+					var damage = randi_range(10,20)
 					body.getHurt(damage)
 					canHit = false
 					$HitCooldown.start()
+	move_and_slide()
 
-func getHurt():
-	canMove = false
+
 	
-	canHit = false
-	await get_tree().create_timer(2.5).timeout
-	queue_free()
 
+func getHurt(Damage : int ):
+	canMove = false
+	canHit = false
+	$HitResultCooldown.start()
+	Health-=Damage
+	if Health <= 0:
+		queue_free()
+
+
+	
 func _on_hit_cooldown_timeout() -> void:
 	canHit = true
+
+func _on_hit_result_cooldown_timeout() -> void:
+	canMove = true
+	canHit = true
+	
