c4de60215ceb4af9dccd216bf0aacefa31fa4534
Egemen12344556f <112466102+Egemen12344556f@users.noreply.github.com>
2025-12-06 07:05:30 +0300

    Final changes -egemen

--- new-game-project/Scenes/Entities/player.gd
+++ new-game-project/Scenes/Entities/player.gd
@@ -39,6 +39,9 @@ func _physics_process(_delta: float) -> void:
 	
 	if Input.is_action_just_pressed("hit"):
 		bite()
+		
+	if Input.is_action_just_pressed("transform"):
+		bite()
 
 func transform():
 		if isVampire:
